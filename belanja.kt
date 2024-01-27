a7870a48caa69f3173f1f8434d518e00849c2208
ChairawanChannel <149566446+ChairawanChannel@users.noreply.github.com>
2024-01-27 11:21:21 +0700

    Update belanja.kt

--- belanja.kt
+++ belanja.kt
@@ -20,15 +20,23 @@ fun total(harga: Int, hargaDiskon: Int): Int{
     
 }
 
+fun hitungDiskon(harga: Int): Double {
+    return when {
+        harga >= 100000 -> 0.1
+        harga >= 80000 -> 0.05
+        else -> 0.0
+    }
+}
+
 fun main() {
     val namaBarang = "Gamis"
-    val diskon = 0.05
     val harga = barang(namaBarang)
-    val hargaDiskon= if (harga>100000) diskon(harga,diskon) else 0
+    val diskon = hitungDiskon(harga)
+    val hargaDiskon = if (diskon > 0) diskon(harga, diskon) else 0
     val hargaTotal = total(harga, hargaDiskon)
+
     println("Baju yang anda beli: \nNama barang\t: $namaBarang \nHarga Barang\t: Rp. $harga")
     println("Diskon \t\t: Rp. $hargaDiskon")
     println("Total Bayar \t: Rp. $hargaTotal")
-    
 }
 
